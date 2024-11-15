6a3033645a977e4e997ca0764c6e647dfdabf7d1
savan5533 <ssangani77@gmail.com>
2024-11-15 11:01:38 +0530

    Changed in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-    output: 'export'  // for static exports (or 'standalone' for SSR)
+    output: 'export', // Ensures the app is exported as a static site
+    images: {
+      unoptimized: true, // Disables image optimization for static exports
+    },
   };
   
   module.exports = nextConfig;
